45539caf8be3215119ff6856cd7b8d8f977687c4
Kamal Singh <kamalsingh23056@gmail.com>
2024-02-24 01:24:56 +0530

    Creating Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import {Box} from "@mui/material";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" exact element={<VideoDetail />} />
+                <Route path="/channel/ :id" exact element={<ChannelDetail />} />
+                <Route path="/search/ :searchTerm" exact element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+  );
 
 export default App
