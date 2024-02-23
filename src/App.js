8712aec69ebb19d565ae0f6950cc37daee494d32
Kamal Singh <kamalsingh23056@gmail.com>
2024-02-24 01:48:48 +0530

    Importing Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import {Box} from "@mui/material";
 
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from "./components";
+
+
 const App = () => (
 
     <BrowserRouter>
         <Box sx={{ backgroundColor: '#000' }}>
-            <Navbar />
+            <Navbar/>
             <Routes>
                 <Route path="/" exact element={<Feed />} />
                 <Route path="/video/:id" exact element={<VideoDetail />} />
-                <Route path="/channel/ :id" exact element={<ChannelDetail />} />
-                <Route path="/search/ :searchTerm" exact element={<SearchFeed />} />
+                <Route path="/channel/:id" exact element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
             </Routes>
         </Box>
     </BrowserRouter>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchFeed = () => {
   return (
-    <div>SearchFeed</div>
+    <div>SearchFeedd</div>
   )
 }
 
