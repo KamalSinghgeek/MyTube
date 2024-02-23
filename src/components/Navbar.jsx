23e49ecdbc3117d9315569f2a1a3bb127288e85e
Kamal Singh <kamalsingh23056@gmail.com>
2024-02-24 02:29:41 +0530

    Add Navbar and Constants file, logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import {logo} from '../utils/constants';
 
-const Navbar = () => {
-  return (
+const Navbar = () => (
     <div>Navbar</div>
   )
-}
 
 export default Navbar
