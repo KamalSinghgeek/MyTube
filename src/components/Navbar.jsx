002b6dcd5f5f20f1fe957ec791cea4477a3e5bc7
Kamal Singh <kamalsingh23056@gmail.com>
2024-02-26 08:53:46 +0530

    Add Navbar and SearchBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import {logo} from '../utils/constants';
+import SearchBar from "./SearchBar";
+
 
 const Navbar = () => (
-    <div>Navbar</div>
+    <Stack 
+    direction="row" 
+    alignItems="center" 
+    p={2} 
+    sx={{position: 'sticky', background: '#000', top:0, justifycontent: 'space-between'}} 
+    >
+      <Link to="/" style={{display:'flex', alighItems: 'center'}}>
+        <img src={logo} alt="logo" height={60} />
+      </Link>
+      <SearchBar />
+    </Stack>
   )
 
 export default Navbar
