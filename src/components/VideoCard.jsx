8994921916fec0a10231663fc1b0f82c37b8bb51
Kamal Singh <kamalsingh23056@gmail.com>
2024-03-03 01:36:36 +0530

    Updates

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,12 +11,14 @@ const VideoCard = ({video:{ id: {
     videoId }, snippet } }) => {
      
   return (
-    <Card sx={{width: {md:'320px', xs:'100%' },
+    <Card sx={{width: {xs:'100%', sm:'358px', md:'320px' },
     boxShadow: 'none', borderRadius:0 }}>
         <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
             <CardMedia image={snippet?.thumbnails?.high?.url}
             alt={snippet?.title}
-            sx= {{width: 358, height:180}}
+        sx= {{width: {
+            xs:'100%', sm:'358px', md:'320px'
+        }, height:180}}
 
             />
         </Link>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,12 @@
 import { Stack, Box } from '@mui/material';
 import {VideoCard, ChannelCard} from './'; 
   
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+
+  if(!videos?.length) return 'Loading...';
   return (
 
-   <Stack direction="row" flexWrap="wrap" 
+   <Stack direction={direction || "row"} flexWrap="wrap" 
    justifyContent="start" gap={2}>
    {videos.map((item, idx) => (
     <Box key={idx}>
