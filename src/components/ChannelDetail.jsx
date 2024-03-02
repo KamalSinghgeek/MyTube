4cfc3f6d77cbcb12033357170c559a1c100bf685
Kamal Singh <kamalsingh23056@gmail.com>
2024-03-02 15:14:34 +0530

    Updates

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -29,10 +29,17 @@ useEffect(() => {
     height:'300px'
     }}
     />
-<ChannelCard channelDetail={channelDetail}/>
-  
+<ChannelCard channelDetail={channelDetail} 
+marginTop="-110px"/>
+
 </Box>
-    </Box>
+<Box display="flex" p="2">
+<Box sx={{mr:{sm:'100px'}}}/>
+<Videos videos={videos}/>
+
+</Box>
+</Box>
+</Box>
   )
 }
 
